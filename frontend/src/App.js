4af2a165a0d866a1c7cca8887b35a11accd2bb19
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-11-26 14:47:38 +0200

    some simple components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 const App = () => {
   return (
     <>
-      <h1>TEST SHOP</h1>
+      <Header />
+      <main>
+        <h1>TEST SHOP</h1>
+      </main>
+      <Footer />
     </>
   )
 }
