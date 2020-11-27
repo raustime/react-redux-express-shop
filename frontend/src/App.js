9d50f0f746f2a14bbc95bdabbfb5e6a8f425db4e
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-11-27 15:12:32 +0200

    Added components and home svreen + products cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>TEST SHOP</h1>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={23} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
