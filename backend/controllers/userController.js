0ee42fed552a44e9d2b0d8c37f781f78825d776f
raustime <raustime@gmail.com>
2020-12-18 16:00:11 +0200

    user authorization and creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,13 +14,62 @@ const authUser=asyncHandler(async(req,res)=>{
            token: generateToken(user._id)
        })
    } else {
-       res.status(401)
+       res.status(404)
        throw new Error("Invalid email or password")
    }
 
 })
 
+const registerUser=asyncHandler(async(req,res)=>{
+    const {name, email, password}=req.body
+    const userExists=await User.findOne({email})
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user=await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+       throw new Error("Invalid user data")
+    }
+ 
+ })
+
+const getUserProfile=asyncHandler(async(req,res)=>{
+   const user=await User.findById(req.user._id)
+
+   if(user ){
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+    })
+   }else {
+    res.status(401)
+    throw new Error("User not found")
+   }
+ 
+ })
+
 
 
 
 exports.authUser=authUser
+exports.registerUser=registerUser
+
+exports.getUserProfile=getUserProfile
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const {authUser} = require('../controllers/userController')
+const { authUser, getUserProfile, registerUser } = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.route('/login').post(authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 
 
