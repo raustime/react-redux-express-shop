a10709636a5b87ed54a0372d5681052a9f357c83
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2021-01-15 16:20:42 +0200

    backend product create and update functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,6 +28,57 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+  } = req.body
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+    product.numReviews = numReviews
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+  res.status(201).json(createdProduct)
+})
+
 //exports.getProducts=getProducts
 //exports.getProductById=getProductById
-module.exports = { getProducts, getProductById, deleteProduct }
+module.exports = {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
